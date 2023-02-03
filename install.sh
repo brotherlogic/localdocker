ea704373eac321cce484d8eea65b4f874319d9a4
Simon Tucker <brotherlogic@gmail.com>
2023-02-02 18:47:23 -0800

    Adds in certs

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 # 2: Password
 
 mkdir certs
-openssl req -x509 -newkey rsa:4096 -days 365 -nodes -sha256 -keyout certs/tls.key -out certs/tls.crt -subj "/CN=registry.brotherlogic-backend.com" -addext "subjectAltName = DNS:registry.brotherlogic.backend.com"
+openssl req -x509 -newkey rsa:4096 -days 365 -nodes -sha256 -keyout certs/tls.key -out certs/tls.crt -subj "/CN=registry.brotherlogic-backend.com" -addext "subjectAltName = DNS:registry.brotherlogic-backend.com"
 
 mkdir auth
 docker run --rm --entrypoint htpasswd registry:2.6.2 -Bbn $1 $2 > auth/htpasswd
