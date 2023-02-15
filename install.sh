6452b8d2197e00aa23ddcc0b85c19822882edbad
Simon Tucker <brotherlogic@gmail.com>
2023-02-14 19:21:50 -0800

    Use nfs mount for pvc

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # 1: User
 # 2: Password
 
-mkdir auth
+mkdir -p auth
 docker run --rm --entrypoint htpasswd registry:2.6.2 -Bbn $1 $2 > auth/htpasswd
  
-kubectl create secret generic auth-secret --from-file=/registry/auth/htpasswd
+kubectl create secret generic auth-secret --from-file=auth/htpasswd
