15c988e47e0419c9114474d928b8257d5980b300
Simon Tucker <brotherlogic@gmail.com>
2023-02-15 19:16:16 -0800

    Fix install script

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@
 mkdir -p auth
 docker run --rm --entrypoint htpasswd registry:2.6.2 -Bbn $1 $2 > auth/htpasswd
  
-kubectl create secret generic auth-secret --from-file=auth/htpasswd
+kubectl create secret generic auth-secret --from-file=auth/htpasswd -n docker
